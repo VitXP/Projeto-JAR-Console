8b5a792b9b922d5baee73a7b0fe8e6ae4a74d9ec
vitXP <vitor.pereira@sptech.school>
2023-03-10 17:22:23 -0300

    Finalização do menu e funcionalidade 1

--- projeto-individual-vitor-xavier/src/main/java/vitor/projeto/individual/vitor/xavier/FigurasGeometricas.java
+++ projeto-individual-vitor-xavier/src/main/java/vitor/projeto/individual/vitor/xavier/FigurasGeometricas.java
@@ -4,11 +4,158 @@
  */
 package vitor.projeto.individual.vitor.xavier;
 
-
 /**
  *
  * @author vitor
  */
 public class FigurasGeometricas {
 
+    void exibirQuadradoTrianguloRetangulo() {
+        System.out.println(String.format("\n              - Figuras Geométricas - \n \n \n"
+                + "+--------------+          /\\          +----------------------+      \n"
+                + "|              |         /  \\         |                      |      \n"
+                + "|              |        /    \\        |                      |      \n"
+                + "|              |       /      \\       |                      |      \n"
+                + "|              |      /        \\      |                      |      \n"
+                + "|              |     /          \\     |                      |      \n"
+                + "|              |    /            \\    |                      |      \n"
+                + "+--------------+    --------------    +----------------------+      \n"
+        )
+        );
+    }
+
+    void exibirQuadrado() {
+        System.out.println(String.format(
+                "  +------------+\n"
+                + "  |            |\n"
+                + "  |            |\n"
+                + "l |            |\n"
+                + "  |            |\n"
+                + "  |            |\n"
+                + "  +------------+\n"
+        )
+        );
+    }
+
+    void exibirTrianguloRetangulo() {
+        System.out.println(String.format(
+                "   |\\\n"
+                + "   | \\\n"
+                + "   |  \\\n"
+                + "   |   \\\n"
+                + " a |    \\  c\n"
+                + "   |     \\\n"
+                + "   |      \\\n "
+                + "  |       \\\n"
+                + "   +--------\n"
+                + "      b   "
+        )
+        );
+    }
+
+    void exibirRetangulo() {
+        System.out.println(String.format(
+                "  +---------------------------------+\n"
+                + "  |                                 |\n"
+                + "  |                                 |\n"
+                + "a |                                 |\n"
+                + "  |                                 |\n"
+                + "  |                                 |\n"
+                + "  |                                 |\n"
+                + "  +---------------------------------+\n"
+                + "               b"
+        )
+        );
+    }
+
+    void separar() {
+        System.out.println("\n\n---------------------------------------------------------------------------------");
+    }
+
+    void separarFinal() {
+        System.out.println("---------------------------------------------------------------------------------\n\n");
+    }
+
+    Double calcularPerimetroQuadrado(Double medidaLadoQuadrado) {
+        return medidaLadoQuadrado * 4;
+    }
+
+    Double calcularAreaQuadrado(Double medidaLadoQuadrado) {
+        return medidaLadoQuadrado * medidaLadoQuadrado;
+    }
+
+    Double calcularPerimetroTriangulo(Double ladoA, Double ladoB, Double ladoC) {
+        return ladoA + ladoB + ladoC;
+    }
+
+    Double calcularAreaTriangulo(Double base, Double altura) {
+        return base * altura / 2;
+    }
+
+    Double calcularPerimetroRetangulo(Double altura, Double base) {
+        return 2 * (altura + base);
+    }
+
+    Double calcularAreaRetangulo(Double altura, Double base) {
+        return altura * base;
+    }
+
+    void ExibirQuadradoSeria() {
+        System.out.println(String.format(
+                "+------------+\n"
+                + "|            |\n"
+                + "|            |\n"
+                + "|            |\n"
+                + "|            |\n"
+                + "|            |\n"  
+                + "+------------+\n"
+        )
+        );
+    }
+
+    void exibirTrianguloSeria() {
+        System.out.println(String.format(
+                "      /\\         \n"
+                + "     /  \\        \n"
+                + "    /    \\       \n"
+                + "   /      \\      \n"
+                + "  /        \\     \n"
+                + " /          \\    \n"
+                + "/            \\   \n"
+                + "--------------    \n"
+        )
+        );
+    }
+
+    void exibirRetanguloSeria() {
+        System.out.println(String.format(
+                "+---------------------------------+\n"
+                + "|                                 |\n"
+                + "|                                 |\n"
+                + "|                                 |\n"
+                + "|                                 |\n"
+                + "|                                 |\n"
+                + "|                                 |\n"
+                + "+---------------------------------+\n"
+               
+        )
+        );
+    }
+
+    void exibirOctogonoSeria() {
+        System.out.println(String.format(
+                "   +----------+\n"
+                + "  /            \\\n"
+                + " /              \\\n"
+                + "+                +\n"
+                + "|                |\n"
+                + "+                +\n"
+                + " \\              /\n"
+                + "  \\            /\n"
+                + "   +----------+\n"
+        ));
+
+    }
+    
+    
 }
--- projeto-individual-vitor-xavier/src/main/java/vitor/projeto/individual/vitor/xavier/TesteFigurasGeometricas.java
+++ projeto-individual-vitor-xavier/src/main/java/vitor/projeto/individual/vitor/xavier/TesteFigurasGeometricas.java
@@ -14,25 +14,19 @@ public class TesteFigurasGeometricas {
 
     public static void main(String[] args) {
 
+        FigurasGeometricas fig = new FigurasGeometricas();
+
         Scanner leitor = new Scanner(System.in);
         Integer operacaoSelecionada = 0;
-        System.out.println(String.format("\n              - Figuras Geométricas - \n \n \n"
-                + "+------------+          /\\          +----------------------+      \n"
-                + "|            |         /  \\         |                      |      \n"
-                + "|            |        /    \\        |                      |      \n"
-                + "|            |       /      \\       |                      |      \n"
-                + "|            |      /        \\      |                      |      \n"
-                + "|            |     /          \\     |                      |      \n"
-                + "|            |    /            \\    |                      |      \n"
-                + "+------------+    --------------    +----------------------+      \n"
-        )
-        );
-
+        fig.exibirQuadradoTrianguloRetangulo();
+        Integer pontuacaoQuiz = 0;
+       
+        
         do {
             System.out.println(String.format(" \n"
                     + "1 - Cálculo (Área e Perímetro)           \n"
-                    + "2 - Gerador De Figuras Geométricas       \n"
-                    + "3 - Qual Figura Geométricas  você seria?   \n"
+                    + "2 - Qual Figura Geométricas  você seria?   \n"
+                    + "3 - Gerador De Figuras Geométricas       \n"
                     + "4 - Quiz (Figuras Geométricas)           \n"
                     + "0 – Sair"));
 
@@ -42,32 +36,117 @@ public class TesteFigurasGeometricas {
 
                 //Cálculo
                 case 1:
+                    fig.separar();
+                    System.out.println(String.format("Digite o número da Figura Geométrica, no qual deseja realizar o cálculo:"));
+                    System.out.println(String.format(
+                            "1 - Quadrado\n"
+                            + "2 - Triangulo Retângulo\n"
+                            + "3 - Retangulo\n"
+                            + "4 - Voltar ao Menu"
+                    ));
+
+                    Integer numeroMenu = leitor.nextInt();
+
+                    switch (numeroMenu) {
+                        case 1:
+                            fig.exibirQuadrado();
+                            System.out.println(String.format("Digite a medida do Lado (L) do quadrado em centímetros"));
+                            Double medidaLadoQuadradoDigitada = leitor.nextDouble();
+
+                            Double perimetroQuadrado = fig.calcularPerimetroQuadrado(medidaLadoQuadradoDigitada);
+                            Double areaQuadrado = fig.calcularAreaQuadrado(medidaLadoQuadradoDigitada);
+
+                            System.out.println(String.format("A área do quadrado é de %.2fcm², e o seu perímetro equivale à %.2fcm.", areaQuadrado, perimetroQuadrado));
+                            fig.separarFinal();
+
+                            break;
+
+                        case 2:
+                            fig.exibirTrianguloRetangulo();
+                            System.out.println(String.format("Digite a medida do Lado A (Altura) do triangulo em centímetros"));
+                            Double medidaLadoATrianguloDigitada = leitor.nextDouble();
+
+                            System.out.println(String.format("Digite a medida do Lado B (Base) do triangulo em centímetros"));
+                            Double medidaLadoBTrianguloDigitada = leitor.nextDouble();
+
+                            System.out.println(String.format("Digite a medida do Lado C (Hipotenusa) do triangulo em centímetros"));
+                            Double medidaLadoCTrianguloDigitada = leitor.nextDouble();
+
+                            Double perimetroTriangulo = fig.calcularPerimetroTriangulo(medidaLadoATrianguloDigitada, medidaLadoBTrianguloDigitada, medidaLadoCTrianguloDigitada);
+                            Double areaTriangulo = fig.calcularAreaTriangulo(medidaLadoBTrianguloDigitada, medidaLadoATrianguloDigitada);
+
+                            System.out.println(String.format("A área do triangulo é de %.2fcm², e o seu perímetro equivale à %.2fcm.", areaTriangulo, perimetroTriangulo));
+                            fig.separarFinal();
+                            break;
+
+                        case 3:
+                            fig.exibirRetangulo();
+                            System.out.println(String.format("Digite a medida do Lado A (Altura) do retângulo em centímetros"));
+                            Double medidaLadoARetanguloDigitada = leitor.nextDouble();
+
+                            System.out.println(String.format("Digite a medida do Lado B (Base) do retângulo em centímetros"));
+                            Double medidaLadoBRetanguloDigitada = leitor.nextDouble();
+
+                            Double perimetroRetangulo = fig.calcularPerimetroRetangulo(medidaLadoARetanguloDigitada, medidaLadoBRetanguloDigitada);
+                            Double areaRetangulo = fig.calcularAreaRetangulo(medidaLadoARetanguloDigitada, medidaLadoBRetanguloDigitada);
+
+                            System.out.println(String.format("A área do retângulo é de %.2fcm², e o seu perímetro equivale à %.2fcm.", areaRetangulo, perimetroRetangulo));
+                            fig.separarFinal();
+                            break;
+
+                        case 4:
+                            fig.separarFinal();
+
+                            break;
+
+                        default:
+                            System.out.println(String.format("Opção inválida!"));
+                            fig.separarFinal();
+                    }
 
                     break;
 
-                //Gerador
+                    
+                    
+                //FiguravcSeria
                 case 2:
-
+                    
+                    Integer pontuacaoQuadrado = 0;
+                    Integer pontuacaoretangulo = 0;
+                    Integer pontuacaoTriangulo = 0;
+                    Integer pontuacaoOctogono = 0;
+                    
                     break;
 
-                //Figura
+                    
+                    
+                    
+                //Gerador
                 case 3:
-
+                    // exp
+                    System.out.println("Digite o seu nome");
+                    System.out.println("Olá xxx, você seria um quadrado: FIGURA");
                     break;
 
+                    
+                    
+                    
                 //Quiz
                 case 4:
 
                     break;
 
+                    
+                    
+                    
                 //Sair
                 case 0:
-                    System.out.println("Até logo! :)");
+                    System.out.println(String.format("Até logo! :)"));
                     break;
 
                 //Default
                 default:
-                    System.out.println("Opção inválida, tente novamente");
+                    System.out.println(String.format("Opção inválida, tente novamente"));
 
             }
         } while (operacaoSelecionada != 0);
